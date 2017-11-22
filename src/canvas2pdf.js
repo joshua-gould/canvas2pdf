b837d62c5a67b7c77026648f39007c48d473c5bb
Joshua Gould <jgould@broadinstitute.org>
2017-11-22 05:36:06 -0500

    check for null regex when parsing font

--- src/canvas2pdf.js
+++ src/canvas2pdf.js
@@ -100,10 +100,18 @@
     var fontValue = '10px Helvetica';
     this.textAlign = 'left';
     this.textBaseline = 'alphabetic';
-
+    var fontRegex = /^\s*(?=(?:(?:[-a-z]+\s*){0,2}(italic|oblique))?)(?=(?:(?:[-a-z]+\s*){0,2}(small-caps))?)(?=(?:(?:[-a-z]+\s*){0,2}(bold(?:er)?|lighter|[1-9]00))?)(?:(?:normal|\1|\2|\3)\s*){0,3}((?:xx?-)?(?:small|large)|medium|smaller|larger|[.\d]+(?:\%|in|[cem]m|ex|p[ctx]))(?:\s*\/\s*(normal|[.\d]+(?:\%|in|[cem]m|ex|p[ctx])))?\s*([-,\'\"\sa-z]+?)\s*$/i;
+    var defaultFontData = {
+      style: 'normal',
+      size: 10,
+      family: 'Helvetica',
+      weight: 'normal'
+    };
     var parseFont = function () {
-      var regex = /^\s*(?=(?:(?:[-a-z]+\s*){0,2}(italic|oblique))?)(?=(?:(?:[-a-z]+\s*){0,2}(small-caps))?)(?=(?:(?:[-a-z]+\s*){0,2}(bold(?:er)?|lighter|[1-9]00))?)(?:(?:normal|\1|\2|\3)\s*){0,3}((?:xx?-)?(?:small|large)|medium|smaller|larger|[.\d]+(?:\%|in|[cem]m|ex|p[ctx]))(?:\s*\/\s*(normal|[.\d]+(?:\%|in|[cem]m|ex|p[ctx])))?\s*([-,\'\"\sa-z]+?)\s*$/i;
-      var fontPart = regex.exec(_this.font);
+      var fontPart = fontRegex.exec(_this.font);
+      if (fontPart === null) {
+        return defaultFontData;
+      }
       var data = {
         style: fontPart[1] || 'normal',
         size: parseInt(fontPart[4]) || 10,
